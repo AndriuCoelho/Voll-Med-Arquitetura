40912febcfcdb3e43ea61cc8f335a7e324baa0fb
Ândriu <andriu_coelho1993@hotmail.com>
2023-09-16 15:16:44 -0300

    aula 4

--- Vollmed/Views/HomeView.swift
+++ Vollmed/Views/HomeView.swift
@@ -43,29 +43,6 @@ struct HomeView: View {
                 do {
                     let response = try await viewModel.getSpecialists()
                     self.specialists = response
-                    
-                    
-                    var listaDeCompras = ListaDeCompras<String, Int>()
-                    
-                    listaDeCompras.adicionarItem("Caneta para quadro branco", quantidade: 3)
-                    listaDeCompras.adicionarItem("Caderno de anotações", quantidade: 1)
-                    listaDeCompras.adicionarItem("Grafite 0.7", quantidade: 2)
-                    
-                    print("lista de compras:")
-                    listaDeCompras.listarItens()
-                    
-                    print("------")
-                    
-                    var listaDeAlimentos = ListaDeCompras<String, Bool>()
-                    
-                    listaDeAlimentos.adicionarItem("Tomate", quantidade: false)
-                    listaDeAlimentos.adicionarItem("Alface", quantidade: true)
-                    listaDeAlimentos.adicionarItem("Banana", quantidade: true)
-                    
-                    listaDeAlimentos.listarItens()
-                    
-                    
-                    
                 } catch {
                     print(error.localizedDescription)
                 }
